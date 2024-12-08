67f176c249270f719c53b27ca7f545b55733845c
Baha Mert Yılmazer <bahamert010@gmail.com>
2024-12-08 03:49:13 +0300

    Add files via upload

---
+++ Prime Numbers.py
@@ -0,0 +1,15 @@
+from time import sleep
+
+number = 2
+
+while True:
+    isItPrime = True
+    for i in range(2, number):
+        if number % i == 0:
+            isItPrime = False
+            break
+
+    if isItPrime:
+        print(number)
+    sleep(0.01)
+    number += 1
